ab77bc6b366443eecf09f097e7c39f419a3d5e38
Casey Doran <cdoran2011@my.fit.edu>
2017-01-19 13:25:11 -0500

    add b&W pebble support

--- src/c/main.c
+++ src/c/main.c
@@ -1,5 +1,8 @@
 #include <pebble.h>
 
+#if defined(PBL_COLOR)
+
+#define COLOR_WINDOW GColorBlack
 #define COLOR_TIME GColorWhite
 #define COLOR_DATE GColorWhite
 #define COLOR_TEMP GColorWhite
@@ -16,13 +19,44 @@
 #define COLOR_DAY_PARTLY GColorPastelYellow
 #define COLOR_DAY_CLOUDY GColorLightGray
 
-#define COLOR_CLOUDY GColorDarkGray
-#define COLOR_RAINY GColorBlue
-#define COLOR_SNOWY GColorWhite
-#define COLOR_SLEETY GColorCeleste
-#define COLOR_PARTLY GColorLightGray
+#define COLOR_RAINY_DAY GColorBlue
+#define COLOR_SNOWY_DAY GColorWhite
+#define COLOR_SLEETY_DAY GColorCeleste
+#define COLOR_RAINY_NIGHT GColorBlue
+#define COLOR_SNOWY_NIGHT GColorWhite
+#define COLOR_SLEETY_NIGHT GColorCeleste
+
+#else 
+
+#define COLOR_WINDOW GColorBlack
+#define COLOR_TIME GColorWhite
+#define COLOR_DATE GColorWhite
+#define COLOR_TEMP GColorWhite
+#define COLOR_STEPS GColorWhite
+#define COLOR_BATT GColorWhite
+
+#define COLOR_24H_LINES GColorBlack
+#define COLOR_24H_CURRENT GColorWhite
+
+#define COLOR_NIGHT GColorLightGray
+#define COLOR_NIGHT_PARTLY GColorLightGray
+#define COLOR_NIGHT_CLOUDY GColorLightGray
+#define COLOR_DAY GColorWhite
+#define COLOR_DAY_PARTLY GColorWhite
+#define COLOR_DAY_CLOUDY GColorWhite
+
+#define COLOR_RAINY_DAY GColorLightGray
+#define COLOR_SNOWY_DAY GColorLightGray
+#define COLOR_SLEETY_DAY GColorLightGray
+#define COLOR_RAINY_NIGHT GColorWhite
+#define COLOR_SNOWY_NIGHT GColorWhite
+#define COLOR_SLEETY_NIGHT GColorWhite
+
+#endif
 
 #define NO_WEATHER_SKY_LINE_WIDTH 4
+#define WIDTH_24H_LINES 1
+#define WIDTH_24H_LINES_CURRENT 3
 
 #define TRI_W 8
 
@@ -127,8 +161,7 @@ static void prv_save_settings() {
 }
 
 
-static GPoint hours(int hour, int w, int h, int b)
-{
+static GPoint hours(int hour, int w, int h, int b){
     w -= b*2;
     h -= b*2;
     switch (hour % 24) {
@@ -335,6 +368,7 @@ static void label_update_proc(Layer* layer, GContext* ctx) {
         GPoint p1 = hours(i, bounds.size.w, bounds.size.h, 17);
         GPoint p2 = hours(i, bounds.size.w, bounds.size.h, 0);
         graphics_context_set_stroke_color(ctx, COLOR_24H_LINES);
+        graphics_context_set_stroke_width(ctx, WIDTH_24H_LINES);
         graphics_draw_line(ctx, p1, p2);
     }
     time_t temp = time(NULL); 
@@ -343,7 +377,7 @@ static void label_update_proc(Layer* layer, GContext* ctx) {
     GPoint p1 = hours(hour, bounds.size.w, bounds.size.h, 17);
     GPoint p2 = hours(hour, bounds.size.w, bounds.size.h, 0);
     graphics_context_set_stroke_color(ctx, COLOR_24H_CURRENT);
-    graphics_context_set_stroke_width(ctx, 3);
+    graphics_context_set_stroke_width(ctx, WIDTH_24H_LINES_CURRENT);
     graphics_draw_line(ctx, p1, p2);
 
 }
@@ -361,80 +395,119 @@ static void forecast_update_proc(Layer* layer, GContext* ctx) {
         return;
     else if (!s_weather_ready && s_space_ready){ //if we have space only, we can draw just the sunrise/sunset ring
         for (int i = 0; i< 24; i++){
-            graphics_context_set_stroke_width(ctx, NO_WEATHER_SKY_LINE_WIDTH);
+            int width = NO_WEATHER_SKY_LINE_WIDTH;
             if (i > s_sunset || i <= s_sunrise){
                 //night color pick
-                graphics_context_set_stroke_color(ctx, COLOR_NIGHT);
+                graphics_context_set_fill_color(ctx, COLOR_NIGHT);
             }else{
                 //day color pick
-                graphics_context_set_stroke_color(ctx, COLOR_DAY);
+                graphics_context_set_fill_color(ctx, COLOR_DAY);
             }
-            int center = NO_WEATHER_SKY_LINE_WIDTH / 2;
-            GPoint p1 = hours(i, w, h, center);
-            GPoint p2 = hours(i+1, w, h, center);
-            graphics_draw_line(ctx, p1, p2);     
+            GPoint p1 = hours(i, w, h, 0);
+            GPoint p2 = hours(i+1, w, h, 0);
+            GRect r1;
+            if (i < 3 || i >= 21) //bottom
+               r1 = GRect(p2.x, p1.y-width, p1.x - p2.x, width);
+            else if (i >=3 && i < 9) //left side
+                r1 = GRect(p1.x, p1.y, width, p2.y - p1.y); 
+            else if (i >= 9 && i < 15) //top
+                r1 = GRect(p1.x, p1.y, p2.x-p1.x, width);
+            else //right side
+                r1 = GRect(p1.x - width, p1.y, width, p2.y - p1.y);
+            
+            graphics_fill_rect(ctx, r1, 0, GCornerNone);
         }
         return;
     }
     else if (!s_space_ready) //need both space and weather to do the whole set of rings...
         return;
     else{
-        //outer (light + temp + clouds) ring
         for (int i = 0; i< 24; i++){
             
+            //outer (light + temp + clouds) ring
             int temp = (forecast_temp_str[((24-hour) + i) % 24] - '0') + 2;
             graphics_context_set_stroke_width(ctx, temp);
-            int center = temp / 2;
             if (i > s_sunset || i <= s_sunrise){
                 //night color pick
                 if (forecast_clouds_str[((24-hour) + i) % 24] == '0')
-                    graphics_context_set_stroke_color(ctx, COLOR_NIGHT);
+                    graphics_context_set_fill_color(ctx, COLOR_NIGHT);
                 else if (forecast_clouds_str[((24-hour) + i) % 24] == '1')
-                    graphics_context_set_stroke_color(ctx, COLOR_NIGHT_PARTLY);
+                    graphics_context_set_fill_color(ctx, COLOR_NIGHT_PARTLY);
                 else
-                    graphics_context_set_stroke_color(ctx, COLOR_NIGHT_CLOUDY);
+                    graphics_context_set_fill_color(ctx, COLOR_NIGHT_CLOUDY);
             }else{
                 //day color pick
                 if (forecast_clouds_str[((24-hour) + i) % 24] == '0')
-                    graphics_context_set_stroke_color(ctx, COLOR_DAY);
+                    graphics_context_set_fill_color(ctx, COLOR_DAY);
                 else if (forecast_clouds_str[((24-hour) + i) % 24] == '1')
-                    graphics_context_set_stroke_color(ctx, COLOR_DAY_PARTLY);
+                    graphics_context_set_fill_color(ctx, COLOR_DAY_PARTLY);
                 else
-                    graphics_context_set_stroke_color(ctx, COLOR_DAY_CLOUDY);
+                    graphics_context_set_fill_color(ctx, COLOR_DAY_CLOUDY);
             }
-            GPoint p1 = hours(i, w, h, center);
-            GPoint p2 = hours(i+1, w, h, center);
-            graphics_draw_line(ctx, p1, p2);     
-        }
-        
-        graphics_context_set_stroke_width(ctx, 1); //reset stroke width
-        
-        for (int i = 0; i < 24; i++) {
+            GPoint p1 = hours(i, w, h, 0);
+            GPoint p2 = hours(i+1, w, h, 0);
+            
+            GRect r1;
+            if (i < 3 || i >= 21) //bottom
+                r1 = GRect(p2.x, p1.y-temp, p1.x - p2.x, temp);
+            else if (i >=3 && i < 9) //left side
+                r1 = GRect(p1.x, p1.y, temp, p2.y - p1.y); 
+            else if (i >= 9 && i < 15) //top
+                r1 = GRect(p1.x, p1.y, p2.x-p1.x, temp);
+            else //right side
+                r1 = GRect(p1.x - temp, p1.y, temp, p2.y - p1.y);
+            
+            graphics_fill_rect(ctx, r1, 0, GCornerNone);  
+            
+            // inner (precip) ring
+                
             //c, r, s, p, _, ? = cloudy, rain, snow, partly cloudy, clear, unknown
-            switch (forecast_precip_type_str[((24-hour) + i) % 24]) {
-                case 'r':
-                    graphics_context_set_stroke_color(ctx, COLOR_RAINY);
-                    break;
-                case 's':
-                    graphics_context_set_stroke_color(ctx, COLOR_SNOWY);
-                    break;
-                case 'l':
-                    graphics_context_set_stroke_color(ctx, COLOR_SLEETY);
-                    break;
-                case '_':
-                case '?':
-                default:
-                    continue; // don't draw clear segments!
-    
+            if (i > s_sunset || i <= s_sunrise){
+                switch (forecast_precip_type_str[((24-hour) + i) % 24]) {
+                    case 'r':
+                        graphics_context_set_fill_color(ctx, COLOR_RAINY_NIGHT);
+                        break;
+                    case 's':
+                        graphics_context_set_fill_color(ctx, COLOR_SNOWY_NIGHT);
+                        break;
+                    case 'l':
+                        graphics_context_set_fill_color(ctx, COLOR_SLEETY_NIGHT);
+                        break;
+                    case '_':
+                    case '?':
+                    default:
+                        continue; // don't draw clear segments!
+                }
+            } else {
+                switch (forecast_precip_type_str[((24-hour) + i) % 24]) {
+                    case 'r':
+                        graphics_context_set_fill_color(ctx, COLOR_RAINY_DAY);
+                        break;
+                    case 's':
+                        graphics_context_set_fill_color(ctx, COLOR_SNOWY_DAY);
+                        break;
+                    case 'l':
+                        graphics_context_set_fill_color(ctx, COLOR_SLEETY_DAY);
+                        break;
+                    case '_':
+                    case '?':
+                    default:
+                        continue; // don't draw clear segments!
+                }
             }
-            int temp = (forecast_temp_str[((24-hour) + i) % 24] - '0') + 2;
             int width = (forecast_precip_intensity_str[((24-hour) + i) % 24] - '0')+2;
-            int center = (width/2) + temp;
-            graphics_context_set_stroke_width(ctx, 1); //reset stroke width in case the next op comes out to 0
-            graphics_context_set_stroke_width(ctx, width);
-            GPoint p1 = hours(i ,w,h,center);
-            GPoint p2 = hours(i + 1,w,h,center);
-            graphics_draw_line(ctx, p1, p2);
+            p1 = hours(i, w, h, temp);
+            p2 = hours(i+1, w, h, temp);
+            if (i < 3 || i >= 21) //bottom
+                r1 = GRect(p2.x, (p1.y - width), p1.x - p2.x, width);
+            else if (i >=3 && i < 9) //left side
+                r1 = GRect(p1.x , p1.y, width, p2.y - p1.y); 
+            else if (i >= 9 && i < 15) //top
+                r1 = GRect(p1.x, p1.y , p2.x-p1.x, width);
+            else //right side
+                r1 = GRect(p1.x - width, p1.y, width, p2.y - p1.y);
+            
+            graphics_fill_rect(ctx, r1, 0, GCornerNone);           
         }
     }
 }
@@ -853,7 +926,8 @@ static void init() {
     });
     window_stack_push(s_main_window, true);
 
-    window_set_background_color(s_main_window, GColorBlack);
+    window_set_background_color(s_main_window, COLOR_WINDOW);
+
 
     Layer* window_layer = window_get_root_layer(s_main_window);
     calculate_perimiter(window_layer);
