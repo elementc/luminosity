55948c3ad750cf6f6286885766265577028c7e4d
Casey Doran <caseyd.doran@gmail.com>
2025-07-28 00:12:41 -0700

    add current wind speed and bearing

--- src/pkjs/openmeteoweatherdata.js
+++ src/pkjs/openmeteoweatherdata.js
@@ -10,6 +10,14 @@ function get_current_temperature(json) {
     return Math.round(json.current.temperature_2m);
 }
 
+function get_current_wind_speed(json){
+    return Math.round(json.current.wind_speed_10m);
+}
+
+function get_current_wind_bearing(json){
+    return Math.round(json.current.wind_direction_10m);
+}
+
 function get_high_temperature(json, start_offset) {
     var high_temperature = -999999.0;
     for (var i = start_offset; i < start_offset + 24; i++) {
@@ -76,16 +84,14 @@ function handleWeatherData() {
 
     var date = new Date();
 
-    var start_offset = date.getHours(); // Start counting from the current hour
-
-    // todo
+    var start_offset = date.getHours(); // Start counting from the current hour (god i hope this is base 24)
 
     // Assemble dictionary using our keys
     var dictionary = {
         'TEMPERATURE': get_current_temperature(json),
         'CONDITIONS': "clear-day", // TODO
-        'WIND_SPEED': 15, // TODO
-        'WIND_BEARING': 45, // TODO
+        'WIND_SPEED': get_current_wind_speed(json),
+        'WIND_BEARING': get_current_wind_bearing(json),
         'FORECAST_CLOUDS': build_cloud_cover_forecast_str(json, start_offset),
         'FORECAST_PRECIP_TYPE': "_________________________", // TODO
         'FORECAST_PRECIP_INTENSITY': "0000000000000000000000000", // TODO
